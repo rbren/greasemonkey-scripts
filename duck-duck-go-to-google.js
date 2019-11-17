99b0e675983827c9cd707e5a31ebeebe33bdcfbf
Robert Brennan <bobby.brennan@gmail.com>
2019-11-17 12:09:05 -0500

    Update duck-duck-go-to-google.js

--- duck-duck-go-to-google.js
+++ duck-duck-go-to-google.js
@@ -6,9 +6,9 @@
 // ==/UserScript==
 setTimeout(function() {
   if (document.getElementById('google_button')) return
-  el = document.getElementById('duckbar_static');
-  query = document.getElementById('search_form_input').value
-  li = document.createElement('li')
+  var el = document.getElementById('duckbar_static');
+  var query = document.getElementById('search_form_input').value
+  var li = document.createElement('li')
   li.id = "google_button"
   li.classList.add("zcm__item")
   li.innerHTML = '<a href="https://google.com/search?q=' + encodeURIComponent(query) + '">GOOGLE IT</a>';
